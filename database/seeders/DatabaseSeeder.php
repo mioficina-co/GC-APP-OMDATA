a370c624bb2324b257b818c25f62486d4481865c
Carlos Desarrollador <github@omdata.cloud>
2025-02-26 16:04:38 -0500

    ajuste de seeder usuario administrador

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Andres Gaitan',
+            'email' => 'andres.gaitan@omdata.cloud',
+            'password' => Hash::make('2025adminA'),
         ]);
 
         $this->call([
