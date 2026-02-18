a2dd209d901a9489ce4967d1c625a2f395255c79
Carlos Desarrollador <carlos.alarcon@omdata.cloud>
2026-02-18 17:42:20 -0500

    fix(nombres-de-tabla): ajuste de nombres de tabla en base de datos

--- app/Models/RazonVisita.php
+++ app/Models/RazonVisita.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class RazonVisita extends Model
 {
     //
+
+    protected $table = 'razonvisitas';
     public function visitas()
     {
         return $this->hasMany(Visitas::class);
